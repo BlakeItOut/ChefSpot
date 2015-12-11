726993b76a4574fa3e01239be96c58febffeab08
Fernando Jones <fernandoajones@gmail.com>
2015-12-10 20:53:56 -0500

    first real combo

--- example.js
+++ example.js
@@ -33,6 +33,7 @@ app.controller('mainCTRL', function ($scope, $sce, $location) {
   $scope.go = function ( path ) {
     $location.path( path );
   }
+  $scope.anything = "text";
 })
 
 app.controller('eatUserProfileCTRL', function ($scope, $sce) {
@@ -71,10 +72,48 @@ app.controller('eatUserProfileCTRL', function ($scope, $sce) {
 app.controller('eatCooksCTRL', function ($scope, $sce) {
 });
 
-app.controller('eatMenuCTRL', function ($scope, $sce) {
+app.controller('eatMenuCTRL', function ($scope, $sce, checkoutFCTRL) {
+  $scope.submitter= function(){
+      checkoutFCTRL.setData($scope.input1, $scope.input2, $scope.input3);}
 });
 
-app.controller('eatCheckoutCTRL', function ($scope, $sce) {
+app.controller('eatCheckoutCTRL', function ($scope, $sce, checkoutFCTRL) {
+    $scope.checkoutFCTRL= checkoutFCTRL;
+    debugger
+    $scope.input1= checkoutFCTRL.input1;
+    $scope.input2= checkoutFCTRL.input2;
+    $scope.input3= checkoutFCTRL.input3;
+    
+    $scope.confirm = function(){
+      var handler = StripeCheckout.configure({
+        key: 'pk_test_s1K2R5T90nTKpXtyZbQtg0o8',
+        // image: '/img/documentation/checkout/marketplace.png',
+        locale: 'auto',
+        token: function(token) {
+          // Use the token to create the charge with a server-side script.
+          // You can access the token ID with `token.id`
+          $http.post("/stripe", token).success(function(data){
+            console.log(data);
+          })
+        }
+      });
+        handler.open({
+          name: checkoutFCTRL.input1,
+          description: checkoutFCTRL.input2,
+          zipCode: true,
+          amount: checkoutFCTRL.input3
+        });
+      }
+});
+
+app.factory('checkoutFCTRL', function(){
+  var pmntInfoCont= {};
+  pmntInfoCont.setData = function(input1, input2, input3){
+        this.input1 = input1; 
+        this.input2 = input2;
+        this.input3 = input3;
+      }
+      return pmntInfoCont;
 });
 
 app.controller('eatPastReservationsCTRL', function ($scope, $sce) {
